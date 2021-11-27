518f3ddb47b699948a9940c8e237fbf65d0c9bf4
Halil İbrahim KURT <halilibrahimkurtt10@gmail.com>
2021-11-28 00:03:22 +0300

    28.11.2021

---
+++ 3-BlackjackGame/index.js
@@ -0,0 +1,44 @@
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+    sumEl.textContent = "Sum:" + sum
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+}
+
+function getRandomCard(){
+    return 5
+}
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
